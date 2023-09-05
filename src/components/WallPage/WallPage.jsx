9e3472843a5970dfac4b5c463cdbffdaa7e2e913
Amir <najiamirdesktop@gmail.com>
2023-09-05 13:03:38 +0330

    update wallpage

--- src/components/WallPage/WallPage.jsx
+++ src/components/WallPage/WallPage.jsx
@@ -7,15 +7,39 @@ function WallPage() {
   return (
     <>
       <div className="flex items-center justify-center text-black h-screen pt-96 mb-72">
-        <div className="flex flex-col pt-10">
-          <img src={wall.largeImageURL} alt="" />
-          <div className="flex gap-40 items-center justify-center">
-            <p>{wall.tags}</p>
-            <p>downloads: {wall.downloads}</p>
-            <p>likes: {wall.likes}</p>
-            <p>comments: {wall.comments}</p>
-            <p>downloads: {wall.views}</p>
+        <div className="flex flex-col pt-32 gap-8 justify-center items-center pb-8">
+          <img
+            className="rounded-lg shadow-lg shadow-teal-700"
+            src={wall.largeImageURL}
+            alt=""
+          />
+          <div
+            className="flex gap-40 items-center justify-center shadow-md
+           shadow-teal-400 ring-1 ring-teal-200 rounded-full p-8 text-teal-700 text-xl"
+          >
+            <p>
+              <span className="font-semibold">{wall.tags}</span>
+            </p>
+            <p>
+              downloads <span className="font-bold">{wall.downloads}</span>
+            </p>
+            <p>
+              likes <span className="font-bold">{wall.likes}</span>
+            </p>
+            <p>
+              comments <span className="font-bold">{wall.comments}</span>
+            </p>
+            <p>
+              views <span className="font-bold">{wall.views}</span>
+            </p>
           </div>
+          <a
+            className="shadow-md hover:shadow-none shadow-teal-500 bg-teal-600 text-white rounded-full font-bold uppercase p-5 hover:bg-teal-500"
+            href={wall.largeImageURL}
+            target="_blank"
+          >
+            download image
+          </a>
         </div>
       </div>
     </>
